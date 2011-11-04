f1dbe4b29521a66a51fbf1938adf957b278e46d4
Rodney Blevins <rodney@blevins.nl>
2011-11-04 16:19:46 +0100

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', :to => 'users#new'
+  
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  
   
   get "pages/home"
   get "pages/contact"
